3bdee1cc2dfa02f05b4d8153f50aee649c91a6f2
msolbes <solbes1918@gmail.com>
2019-01-30 17:44:19 +0100

    Just testing my first project in github

---
+++ P0/dna_count_file.py
@@ -0,0 +1,17 @@
+f = open("dna.txt", "r")
+seq = f.read()
+f.close()
+
+seq = seq.replace("\n","").replace(" ","")
+total_lenght = len(seq)
+
+num_A = seq.count("A")
+num_C = seq.count("C")
+num_G = seq.count("G")
+num_T = seq.count("T")
+
+print("The total lenght of the sequence is:", total_lenght)
+print("The character A appears ", num_A, "times")
+print("The character C appears", num_C, "times")
+print("The chatacter G appears", num_G, "times")
+print("The character T appears", num_T, "times")
