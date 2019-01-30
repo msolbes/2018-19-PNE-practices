f142275d5dfed3013e11344f568f4938200cd09c
msolbes <solbes1918@gmail.com>
2019-01-30 17:26:32 +0100

    Just testing my first project in github

--- P0/dna_count.py
+++ P0/dna_count.py
@@ -1,3 +1,7 @@
-sequence = input("Please introduce a valid DNA sequence: ")
+sequence = input("Please introduce a valid DNA sequence: ").upper()
 
-print(len(sequence))
+print("The total length of the quence is: ", len(sequence))
+print("The character A appears ", sequence.count("A"), "times")
+print("The character C appears ", sequence.count("C"), "times")
+print("The character G appears ", sequence.count("G"), "times")
+print("The character T appears ", sequence.count("T"), "times")
