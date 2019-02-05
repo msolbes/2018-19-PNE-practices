dff4123c6846af35ad088eca7b9aace15823c58e
msolbes <solbes1918@gmail.com>
2019-02-05 15:50:30 +0100

    Division by zero error found and fixed

--- session-5/ex-1.py
+++ session-5/ex-1.py
@@ -9,7 +9,7 @@ def count_a(seq):
 
     return result
 #Main program
-s = "AGTACACTGGT"
+s = input("please enter the sequence: ")
 na = count_a(s)
 print("The number of As is: {}".format(na))
 
@@ -17,7 +17,11 @@ print("The number of As is: {}".format(na))
 tl = len(s)
 
 #Calculate the percentage of As in the sequence
-perc = round(100.0 * na/tl,1)
+if tl > 0:
+    perc = round(100.0 * na/tl,1)
+
+else:
+    perc = 0
 
 print("The total lenght is: {}".format(tl))
 print("The percentage of As is : {}%".format(perc))
