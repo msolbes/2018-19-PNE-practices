b98a182d14e9b2504c861b4e42967dc43414619e
msolbes <solbes1918@gmail.com>
2019-02-06 18:30:18 +0100

    havent finished

--- session-5/exercise-1.py
+++ session-5/exercise-1.py
@@ -1,4 +1,4 @@
-ef count_a(seq):
+def count_let(seq):
     """Computing the number of As in the sequence"""
     result_a = 0
     result_c = 0
@@ -8,31 +8,40 @@ ef count_a(seq):
     for b in seq:
         if b == 'A':
             result_a += 1
-
         elif b == 'C':
-            result_g +=1
-
+            result_c +=1
         elif b == 'G':
             result_g +=1
         elif b == 'T':
             result_t +=1
 
+        dic = {"As": result_a, "Cs": result_c, "Gs": result_g, "Ts": result_t}
+    return dic
 
-    return result
 #Main program
-s = input("please enter the sequence: ")
-na = count_a(s)
-print("The number of As is: {}".format(na))
+s = input("please enter the sequence: ").upper()
+n_let = count_let(s)
+print("The letters of the sequence are: {}".format(n_let))
 
 #Calculate the total sequence length
 tl = len(s)
 
 #Calculate the percentage of As in the sequence
 if tl > 0:
-    perc = round(100.0 * na/tl,1)
+    perc_a = round(100.0 * n_let['As'] /tl, 1)
+    perc_c = round(100.0 * n_let['Cs'] / tl, 1)
+    perc_g = round(100.0 * n_let['Gs'] / tl, 1)
+    perc_t = round(100.0 * n_let['Ts'] / tl, 1)
 
 else:
-    perc = 0
+    perc_a = 0
+    perc_c = 0
+    perc_g = 0
+    perc_t = 0
+
 
 print("The total lenght is: {}".format(tl))
-print("The percentage of As is
+print("The percentage of As is : {}%".format(perc_a))
+print("The percentage of Cs is : {}%".format(perc_c))
+print("The percentage of Gs is : {}%".format(perc_g))
+print("The percentage of Ts is : {}%".format(perc_t))
