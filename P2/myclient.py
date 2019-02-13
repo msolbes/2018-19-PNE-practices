f24437ebf17f53ca200d77328cc7a0fa48990b2c
msolbes <solbes1918@gmail.com>
2019-02-13 18:43:18 +0100

    havent finished

--- P2/myclient.py
+++ P2/myclient.py
@@ -7,7 +7,9 @@ class Seq:
 
 
     def reverse(self):
-        rev = self.strbases()
+        reverse-seq = self.strbases()
+
+    return reverse-seq
 
 
 
@@ -25,9 +27,18 @@ class Seq:
 str1= s1.strbases
 str2= s2.strbases
 
+import socket
 
 PORT = 1112
 IP = "212.128.253.70"
+
+condition= True
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, PORT))
+    s.send(str.encode(seq)))
+
+    seq = input("Please enter a valid sequence")
 
 print("Sequence 1: {}".format(str1))
 print("Sequence 2: {}".format(str2))
