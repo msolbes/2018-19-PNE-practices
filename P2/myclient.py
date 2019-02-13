abb49cf5ec8ec493893657609d6099031e615e47
msolbes <solbes1918@gmail.com>
2019-02-13 18:12:58 +0100

    havent finished

---
+++ P2/myclient.py
@@ -0,0 +1,34 @@
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        print("New empty sequence created")
+
+        self.strbases = strbases
+
+
+    def reverse(self):
+        rev = self.strbases()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+str1= s1.strbases
+str2= s2.strbases
+
+
+PORT = 1112
+IP = "212.128.253.70"
+
+print("Sequence 1: {}".format(str1))
+print("Sequence 2: {}".format(str2))
+print("the end")
