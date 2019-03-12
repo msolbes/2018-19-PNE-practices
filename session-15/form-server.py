4d275653a43940f55fe4745580a66d02f3785000
msolbes <solbes1918@gmail.com>
2019-03-12 16:51:24 +0100

    Learning to use html

---
+++ session-15/form-server.py
@@ -0,0 +1,38 @@
+import http.server
+import socketserver
+import termcolor
+
+PORT = 8002
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+    # -- printing the request line
+
+        termcolor.cprint(self.requestline, 'green')
+
+        f = open("form1.html", 'r')
+        contents = f.read()
+
+        self.send_response(200) # everything is OK (happy server)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+        self.end_headers()
+
+        # -- Sending the body of the response message
+        self.wfile.write(str.encode(contents))
+
+
+# -- Main program
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+    print("Serving at PORT: {}".format(PORT))
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+print("The server is stopped")
