032e10dfc38d90c96cfeb48ac923a599a186edcd
msolbes <solbes1918@gmail.com>
2019-04-08 19:01:44 +0200

    Learning to count the letters in a sequence from a file

--- P0/fibonacci.py
+++ P0/fibonacci.py
@@ -1,19 +1,24 @@
 # Learning the fibonacci sequence
-nterms = int(input("How many terms would you like?"))
-n1 = 0
-n2 = 1
-count = 0
+nterms = int(input("How many terms would you like?: "))
 
-if nterms <= 0:
-    print("Please enter a positive number")
-elif nterms == 1:
-    print(n1)
-else:
-    print("Fibonacci sequence with", nterms, ":")
-    while count < nterms:
-        print(n1, end=' ,')
-        nth = n1 + n2
 
-        n1 = n2
-        n2 = nth
-        count += 1
+def fibonacci(term):
+    n1 = 0
+    n2 = 1
+    list_terms = []
+    if nterms <= 0:
+        print("Please enter a positive number")
+    elif nterms == 1:
+        print(n1)
+    else:
+        print("Fibonacci sequence with", nterms, ":")
+        for i in range(term):
+            list_terms.append(n1)
+            n1 = n2
+            n2 = n1+n2
+        return list_terms
+
+
+fib_series = fibonacci(nterms)
+
+print('The final series is: ', fib_series)
