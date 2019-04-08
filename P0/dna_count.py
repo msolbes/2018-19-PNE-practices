779e04f62c3154dd19c0e935d2fbed5138620aad
msolbes <solbes1918@gmail.com>
2019-04-08 18:58:56 +0200

    Just fixing some things

--- P0/dna_count.py
+++ P0/dna_count.py
@@ -2,7 +2,7 @@
 sequence = input("Please introduce a valid DNA sequence: ").upper()
 
 print("The total length of the quence is: ", len(sequence))
-print("The character A appears ", sequence.count("A"), "times")
-print("The character C appears ", sequence.count("C"), "times")
-print("The character G appears ", sequence.count("G"), "times")
-print("The character T appears ", sequence.count("T"), "times")
+print("The character A appears ", sequence.count("A"), "times.")
+print("The character C appears ", sequence.count("C"), "times.")
+print("The character G appears ", sequence.count("G"), "times.")
+print("The character T appears ", sequence.count("T"), "times.")
