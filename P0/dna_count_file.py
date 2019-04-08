1633e25a5e69aeded858a2c3e05f6344435ab1af
msolbes <solbes1918@gmail.com>
2019-04-08 19:00:50 +0200

    Learning to count the letters in a sequence from a file

--- P0/dna_count_file.py
+++ P0/dna_count_file.py
@@ -1,18 +1,20 @@
 # Learning to count the letters in a sequence from a file
-f = open("dna.txt", "r")
-seq = f.read()
-f.close()
+file_name = input("Name of the file")
+with open(file_name, "r") as f:
+    seq = ""
+    for line in f:
+        line = line.replace("\n", "").replace(" ", "")
+        seq += line
+    total_length = len(seq)
+    seq = seq.upper()
 
-seq = seq.replace("\n", "").replace(" ", "")
-total_lenght = len(seq)
+    num_A = seq.count("A")
+    num_C = seq.count("C")
+    num_G = seq.count("G")
+    num_T = seq.count("T")
 
-num_A = seq.count("A")
-num_C = seq.count("C")
-num_G = seq.count("G")
-num_T = seq.count("T")
-
-print("The total lenght of the sequence is:", total_lenght)
-print("The character A appears ", num_A, "times")
-print("The character C appears", num_C, "times")
-print("The chatacter G appears", num_G, "times")
-print("The character T appears", num_T, "times")
+    print("The total length of the sequence is:", total_length)
+    print("The character A appears ", num_A, "times")
+    print("The character C appears", num_C, "times")
+    print("The character G appears", num_G, "times")
+    print("The character T appears", num_T, "times")
